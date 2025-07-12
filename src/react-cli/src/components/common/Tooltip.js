45ec6b07efa301592240d150602838ffd7030733
malservet nicolas <nicolas@malservet.eu>
2025-07-12 09:46:44 +0200

    nice tooltip easier to maintain

--- src/react-cli/src/components/common/Tooltip.js
+++ src/react-cli/src/components/common/Tooltip.js
@@ -1,23 +1,15 @@
-import React, {  useState } from "react";
-import {MessageCircleQuestion} from 'lucide-react';
+import React from "react";
+import { MessageCircleQuestion } from 'lucide-react';
 /**
  * button to present the main action ( most visible)
  */
 
- export default function Tooltip({name,content}) {
-	const [showContent,setShowContent] =useState(false);
-	
-	function onClick(){
-		setShowContent(!showContent);
-	}
-	
-	function hide(){
-		setShowContent(false);
-	}
-	 
-	 return (<div>
-		<button name={name} className="font-bold py-1 px-3 rounded" onClick={onClick}><MessageCircleQuestion size={18} color={!showContent?"orange":"green"}/></button>
-		{showContent&&<div role="tooltip" className="text-sm font-medium rounded-lg border bg-blue-100">
-			<div className="m-1 ">{content} </div></div>}
-		</div>);
-	 }
+export default function Tooltip({ name, content }) {
+
+
+	return (<div className="relative inline-block group">
+		<div className="text-blue-500"><button name={name} className="font-bold py-1 px-3 rounded" ><MessageCircleQuestion size={18} color={"orange"} /></button>
+		</div>
+		<div className="absolute z-10 hidden group-hover:block bottom-full left-1/2 transform -translate-x-1/2 mb-2 px-2 py-1 leading-none t whitespace-no-wrap rounded shadow-lg text-sm font-medium rounded-lg border bg-blue-100">{content}</div>
+	</div>)
+}

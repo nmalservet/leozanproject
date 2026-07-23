76058eb74eff065fe61bf59964814a172c22d434
malservet nicolas <nicolas@malservet.eu>
2026-07-23 11:48:28 +0200

    view only deployed questionnaire

--- src/react-cli/src/components/business/SurveysSelectList.js
+++ src/react-cli/src/components/business/SurveysSelectList.js
@@ -14,7 +14,7 @@ export default function SurveysSelectList({projectId,selected, onSelection }) {
 
 	function loadData() {
 		var filter ={};
-		//filter.status=2;//status deployed for the surveys on select list
+		filter.status=2;//status deployed for the surveys on select list
 		//filter.project=projectId;//by default we filter by a project id
 		console.log("get surveys")
 		Api.getSurveys(filter).then((response) => {
